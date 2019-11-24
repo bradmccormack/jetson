b9241ffee5d7aafe63fcf271cc1c9d39c59d90f7
Brad McCormack <bradmccormack100@gmail.com>
2019-11-25 06:03:33 +1100

    Refactor + Shellcheck fixes.
    - Refactor code with functions.
    - Address all ShellCheck warnings.
    - Handle sourcing versus direct execution (so single functions can be
    executed after sourcing).

--- nano_desktop.sh
+++ nano_desktop.sh
@@ -1,76 +1,119 @@
 #!/usr/bin/env bash
 
-set -e
+# Simple script to setup the Nvidia Nano for desktop use.
 
 # Inspired by - https://syonyk.blogspot.com/2019/04/nvidia-jetson-nano-desktop-use-kernel-builds.html?m=1
 
 # TODO 
-# Shellcheck
-# Refactor with functions etc
 # Setup the filesystem and partitions
 
+function download_kernel_sources()
+{
+	pushd "${1}"
+	echo "Downloading kernel sources from Nvidia to ${1}"
+	wget -O public_sources.tbz2 https://developer.nvidia.com/embedded/dlc/public_sources_Nano 
+	tar -xf public_sources.tbz2
+	tar -xf public_sources/kernel_src.tbz2
+	pushd kernel/kernel-4.9
+}
 
-pushd /tmp
-echo "Downloading kernel sources from Nvidia"
-wget -O public_sources.tbz2 https://developer.nvidia.com/embedded/dlc/public_sources_Nano 
-tar -xf public_sources.tbz2
-tar -xf public_sources/kernel_src.tbz2
-pushd kernel/kernel-4.9
+function configure_patch_kernel()
+{
+	echo "Applying current running kernel config"
+	zcat /proc/config.gz > .config
 
-echo "Applying current running kernel config"
-zcat /proc/config.gz > .config
+	echo "Copying current Tegra firmware into new kernel build env"
+	cp /lib/firmware/tegra21x_xusb_firmware ./firmware/
 
-echo "Copying current Tegra firmware into new kernel build env"
-cp /lib/firmware/tegra21x_xusb_firmware ./firmware/
+	echo "Updating kernel configuration to support zswap"
 
-echo "Updating kernel configuration to support zswap"
-#Select: Enable frontswap to cache swap pages if tmem is present
-sed -i '/CONFIG_FRONTSWAP/c\CONFIG_FRONTSWAP=y' .config
+	#Select: Enable frontswap to cache swap pages if tmem is present
+	sed -i '/CONFIG_FRONTSWAP/c\CONFIG_FRONTSWAP=y' .config
 
-#Select: Compressed cache for swap pages (EXPERIMENTAL) (NEW)
-sed -i '/CONFIG_ZPOOL/c\CONFIG_ZPOOL=y' .config
+	#Select: Compressed cache for swap pages (EXPERIMENTAL) (NEW)
+	sed -i '/CONFIG_ZPOOL/c\CONFIG_ZPOOL=y' .config
 
-#Select: Low (Up to 2x) density storage for compressed pages
-sed -i '/CONFIG_ZBUD/c\CONFIG_ZBUD=y' .config
+	#Select: Low (Up to 2x) density storage for compressed pages
+	sed -i '/CONFIG_ZBUD/c\CONFIG_ZBUD=y' .config
 
-# Enable the extra firmware blobs
-sed -i '/CONFIG_EXTRA_FIRMWARE/c\CONFIG_EXTRA_FIRMWARE=tegra21x_xusb_firmware' .config
+	# Enable the extra firmware blobs
+	sed -i '/CONFIG_EXTRA_FIRMWARE/c\CONFIG_EXTRA_FIRMWARE=tegra21x_xusb_firmware' .config
 
-# This wasn't part of the default config. Enabling the extra firmware adds this.
-# TODO - make a function to add or update keys and values
-if [ ! grep 'CONFIG_EXTRA_FIRMWARE_DIR' .config ]; then
-  echo 'CONFIG_EXTRA_FIRMWARE_DIR=firmware' >> .config
+	# This wasn't part of the default config. Enabling the extra firmware adds this.
+	# TODO - make a function to add or update keys and values
+	if ! grep "CONFIG_EXTRA_FIRMWARE_DIR" ".config" ; then
+	  echo 'CONFIG_EXTRA_FIRMWARE_DIR=firmware' >> .config
+	else
+	  sed -i '/CONFIG_EXTRA_FIRMWARE_DIR/c\CONFIG_EXTRA_FIRMWARE_DIR=firmware' .config
+	fi
+
+	echo "Patching zswap ..."
+	PATCH_SRC=a85f878b443f8d2b91ba76f09da21ac0af22e07f.patch
+	wget "https://github.com/torvalds/linux/commit/${PATCH_SRC}"
+	patch -p1 < "${PATCH_SRC}"
+
+	sed -i 's/memset_l(page, value, PAGE_SIZE \/ sizeof(unsigned long));/memset(page, value, PAGE_SIZE);/g' mm/zswap.c
+}
+
+function build_kernel()
+{
+	echo "Building custom kernel .."
+
+	NPROC=$(grep -c processor < /proc/cpuinfo )
+	NPROC=$((++NPROC))
+	printf "\nUsing %d cores.\n" "${NPROC}"
+	
+	# This uses the current config but sets all the answers to the default value (so we don't get prompted)
+	make olddefconfig
+
+	# Build using all cores + 1 (recommended)
+	make -j${NPROC}
+
+	# Install the modules
+	sudo make modules_install
+}
+
+function install_kernel()
+{
+    # Back up the old kernel.
+	sudo cp /boot/Image /boot/Image.dist
+
+	# Copy the new kernel into the boot directory
+	sudo cp arch/arm64/boot/Image /boot
+}
+
+function abort()
+{
+	echo "Caught signal."
+	if [ -n "${KERNEL_SOURCE_NO_DELETE}" ]; then
+		echo "KERNEL_SOURCE_NO_DELETE specified. Not cleaning up source."
+	else
+		echo "KERNEL_SOURCE_NO_DELETE not specified. Removing kernel source ..."
+		rm -rf "{KERNEL_SOURCE_PATH}"
+	fi
+
+	echo "Aborted."
+}
+
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+	set -e
+
+	trap abort SIGHUP SIGINT SIGTERM
+
+	KERNEL_SOURCE_PATH="${KERNEL_SOURCE_PATH:-/tmp}"
+	download_kernel_sources "${KERNEL_SOURCE_PATH}"
+	
+	configure_patch_kernel
+	build_kernel
+	
+	install_kernel
+
+	popd
+	popd
+	echo "Done - reboot !"
+	set +e
 else
-  sed -i '/CONFIG_EXTRA_FIRMWARE_DIR/c\CONFIG_EXTRA_FIRMWARE_DIR=firmware' .config
+	echo "Script sourced. Call functions manually."
 fi
 
-echo "Patching zswap ..."
-PATCH_SRC=a85f878b443f8d2b91ba76f09da21ac0af22e07f.patch
-wget "https://github.com/torvalds/linux/commit/${PATCH_SRC}"
-patch -p1 < "${PATCH_SRC}"
-
-
-sed -i 's/memset_l(page, value, PAGE_SIZE \/ sizeof(unsigned long));/memset(page, value, PAGE_SIZE);/g' mm/zswap.c
-
-echo "Building custom kernel"
-
-# This uses the current config but sets all the answers to the default value (so we don't get prompted)
-make olddefconfig
-
-# Build using all cores + 1 (recommended)
-make -j5
-
-# Install the modules
-sudo make modules_install
-
-# Backing up the old boot image ?
-sudo cp /boot/Image /boot/Image.dist
-
-# Copy the new kernel into the boot directory
-sudo cp arch/arm64/boot/Image /boot
-
-popd
-popd
-echo "Done - reboot !"
-
-# TODO setting up the file system and copying over the root filesystem
