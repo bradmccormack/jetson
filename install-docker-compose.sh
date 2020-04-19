bf81d9810ac19ffba76db48615f8e09559e39403
Brad McCormack <bradmccormack100@gmail.com>
2020-04-19 10:48:10 +1000

    Add missing dependency for docker-compose
    - Not related to zram, but docker functionality was one of the main
    reasons I needed to look into improving zram for my environment.

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -3,5 +3,5 @@
 # Installs Docker compose on the Nano
 sudo apt update
 sudo apt install libgcc-7-dev libssl-dev libffi-dev python-dev -y
-sudo -H pip install docker-compose
+sudo -H pip install docker-compose urllib3
 sudo apt autoremove -y
